9b358f6ad8896d16b0d0490e25e44d9e45ce0812
Entrepreneur Connect Club <ecclub@binghamton.edu>
2025-09-05 16:36:07 +0000

    all the text is touching the both end of the screen which does not look

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,11 @@ export default function RootLayout({
       <body className="font-body antialiased bg-background">
         <div className="flex flex-col min-h-screen">
           <Header />
-          <main className="flex-grow">{children}</main>
+          <main className="flex-grow">
+            <div className="container mx-auto px-4">
+              {children}
+            </div>
+          </main>
           <Footer />
         </div>
         <Toaster />
